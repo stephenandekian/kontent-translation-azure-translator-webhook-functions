c16258d4b05c486c44cfecca96e560ec730b45c5
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-11-13 08:55:21 -0500

    Expose snippet+element codenames as env variables

--- Helpers/constants.ts
+++ Helpers/constants.ts
@@ -4,8 +4,8 @@ export namespace constants {
   export const kontentDefaultLanguageId: string = '00000000-0000-0000-0000-000000000000'
   export const kontentManagementApiKey: string = process.env['KontentManagementApiKey']
   export const kontentProjectId: string = process.env['KontentProjectId']
-  export const kontentTranslationSnippetCodename: string = 'translation'
-  export const kontentTranslationElementCodename: string = 'settings'
+  export const kontentTranslationSnippetCodename: string = process.env['KontentTranslationSnippetCodename']
+  export const kontentTranslationElementCodename: string = process.env['KontentTranslationElementCodename']
   export const kontentWorkflowStepIdPublished: string = process.env['KontentPublishedWorkflowStepId']
   export const kontentWorkflowStepIdTranslationPending: string = process.env['KontentTranslationPendingWorkflowStepId']
   export const kontentWorkflowStepIdTranslationReview: string = process.env['KontentTranslationReviewWorkflowStepId']
